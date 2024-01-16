7c8c991b0724445903b456887c7f76d442e2917f
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-01-16 21:15:15 +0000

    fix typo in insert_susbscriber base span name

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,7 +10,7 @@ pub struct FormData {
 }
 
 #[tracing::instrument(
-    name = "Saving new susbscriber details in the database",
+    name = "Saving new subscriber details in the database",
     skip(pool, form)
 )]
 pub async fn insert_susbscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
