8bde0d8e7791c31e416c6d60b8f8b619f672bbdf
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-02-14 17:10:10 +0000

    implement first iteration of subscriber validation

---
+++ src/domain/subscriber_name.rs
@@ -0,0 +1,74 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+#[derive(Debug)]
+pub struct SubscriberName(String);
+
+const FORBIDDEN_CHARS: &[char] = &['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+
+impl SubscriberName {
+    pub fn parse(s: String) -> Result<SubscriberName, String> {
+        let is_empty_or_whitespace = s.trim().is_empty();
+
+        // s must be less than 256 grapheme clusters.
+        let is_too_long = s.graphemes(true).nth(256).is_some();
+
+        let contains_forbidden_chars = s.chars().any(|g| FORBIDDEN_CHARS.contains(&g));
+
+        if is_empty_or_whitespace || is_too_long || contains_forbidden_chars {
+            Err(format!("{} is not a valid subscriber name", s))
+        } else {
+            Ok(Self(s))
+        }
+    }
+}
+
+impl AsRef<str> for SubscriberName {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use claim::{assert_err, assert_ok};
+
+    use super::{SubscriberName, FORBIDDEN_CHARS};
+
+    #[test]
+    fn a_256_graphemes_long_name_is_valid() {
+        let name = "ë".repeat(256);
+        assert_ok!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn a_name_longer_than_256_graphemes_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let name = " ".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let name = "".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn names_containing_invalid_char_are_rejected() {
+        for name in FORBIDDEN_CHARS {
+            let name = name.to_string();
+            assert_err!(SubscriberName::parse(name));
+        }
+    }
+
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        let name = "Francisco".to_string();
+        assert_ok!(SubscriberName::parse(name));
+    }
+}
