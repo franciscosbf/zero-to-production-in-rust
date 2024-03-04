29ce892eeb0914d3b9c53d20b30e8ac8d2fefb0d
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-03-04 15:02:10 +0000

    add status column to subscriptions table

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -32,8 +32,8 @@ pub async fn insert_susbscriber(
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
+        INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+        VALUES ($1, $2, $3, $4, 'confirmed')
         "#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
